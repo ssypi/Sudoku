a2d253ac1f966759afcb6c04cc8821526230f06b
somename <someemail>
2015-10-01 12:49:38 +0300

    besouro automatic message

---
+++ src/org/univoulu/tol/sqatlab/sudoku/SubGrid.java
@@ -0,0 +1,11 @@
+package org.univoulu.tol.sqatlab.sudoku;
+
+import java.util.List;
+
+public class SubGrid {
+	private final List<Integer> cells;
+	
+	public SubGrid(String subgrid) {
+		
+	}
+}
--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -2,12 +2,13 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 public class SudokuVerifier {
 
-	public static final int RESULT_CORRECT = 0;
+	public static final int SUCCESS = 0;
+	public static final int FAIL_SUBGRID_CONTAINS_MULTIPLES = -2;
 
 	/**
 	 * Verifies the given sudoku solution
 	 * @param candidateSolution 81 characters long String where first 9 are the first row, second 9 are the second row etc.
-	 * @return {@link #RESULT_CORRECT} if solution is correct, else less than 0
+	 * @return {@link #SUCCESS} if solution is correct, else less than 0
 	 */
 	public int verify(String candidateSolution) {
 		if (candidateSolution == null || candidateSolution.length() != 81) {
@@ -18,7 +19,7 @@ public class SudokuVerifier {
 		}
 		
 		if (candidateSolution.equals("417369825632158947958724316825437169791586432346912758289643571573291684164875293")) {
-			return RESULT_CORRECT;
+			return SUCCESS;
 		} else {
 			return -1;
 		}
