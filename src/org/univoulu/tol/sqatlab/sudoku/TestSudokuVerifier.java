cce519cfa3703489e0e868ffdcb2a92fa75b9888
somename <someemail>
2015-10-01 11:43:01 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -6,9 +6,19 @@ import org.junit.Test;
 
 public class TestSudokuVerifier {
 
+	private final String SOLUTION_CORRECT = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	private final String SOLUTION_INCORRECT = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void should_accept_correct_solution() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		int result = verifier.verify(SOLUTION_CORRECT);
+		assertEquals(1, result);		
 	}
 
 }
