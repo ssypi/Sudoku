2dc2a77b6a3f29b92c262a0220ef043f8b2ed19b
somename <someemail>
2015-10-01 11:59:40 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -2,11 +2,12 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
 
-	private final String SOLUTION_CORRECT = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	private final String SOLUTION_VALID = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	private final String SOLUTION_INCORRECT = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 	
 	private SudokuVerifier verifier;
@@ -17,15 +18,15 @@ public class TestSudokuVerifier {
 	}
 	
 	@Test
-	public void should_accept_correct_solution() {
-		int result = verifier.verify(SOLUTION_CORRECT);
+	public void should_return_zero_for_valid_solution() {
+		int result = verifier.verify(SOLUTION_VALID);
 		assertEquals(SudokuVerifier.RESULT_CORRECT, result);		
 	}
 	
 	@Test
-	public void should_fail_on_incorrect_solution() {
+	public void should_return_less_than_zero_for_incorrect_solution() {
 		int result = verifier.verify(SOLUTION_INCORRECT);
-		assertEquals(SudokuVerifier.RESULT_INCORRECT, result);
+		assertTrue(result < 0);
 	}
 
 }
