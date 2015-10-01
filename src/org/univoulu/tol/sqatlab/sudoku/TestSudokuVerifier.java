e03df1effd83dcfe459f8e475dc41a884366ff9d
somename <someemail>
2015-10-01 11:50:35 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -9,11 +9,23 @@ public class TestSudokuVerifier {
 	private final String SOLUTION_CORRECT = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	private final String SOLUTION_INCORRECT = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 	
+	private SudokuVerifier verifier;
+	
+	@Before
+	public void setup() {
+		verifier = new SudokuVerifier();
+	}
+	
 	@Test
 	public void should_accept_correct_solution() {
-		SudokuVerifier verifier = new SudokuVerifier();
 		int result = verifier.verify(SOLUTION_CORRECT);
 		assertEquals(SudokuVerifier.RESULT_CORRECT, result);		
+	}
+	
+	@Test
+	public void should_fail_on_incorrect_solution() {
+		int result = verifier.verify(SOLUTION_INCORRECT);
+		assertEquals(SudokuVerifier.RESULT_INCORRECT, result);
 	}
 
 }
